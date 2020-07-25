bc204c34dde90c463b01a1f66301fec20450d02e
Aeop <irmxj777@gmail.com>
2020-07-25 23:32:10 +0900

    semaphore example , cricical section mutex example add

---
+++ os_system_programming/Use_Semaphore/Use_Semaphore.cpp
@@ -0,0 +1,117 @@
+﻿#include <stdio.h>
+#include <tchar.h>
+#include <time.h>
+#include <Windows.h>
+#include <process.h>
+#include <array>
+
+
+inline namespace
+{
+	using uint32 = unsigned int;
+
+	static constexpr uint32 NUM_OF_CUSTOMER{ 50 };
+	static constexpr uint32 RANGE_MIN{ 10 };
+	static constexpr uint32 RANGE_MAX{ 30 - RANGE_MIN };
+	static constexpr uint32 TABLE_CNT{ 10 };
+
+	static HANDLE hSemaphore;
+	static std::array<DWORD, NUM_OF_CUSTOMER> randTimeArr;
+};
+
+void TakeMeal(const DWORD time)
+{
+	WaitForSingleObject(hSemaphore, INFINITE);
+
+	_tprintf(_T("Enter Customer %d~ \n"), GetCurrentThreadId());
+
+	_tprintf(_T("Customer %d having launch~ \n"), GetCurrentThreadId());
+
+	Sleep(1000 * time);
+
+	ReleaseSemaphore(hSemaphore, 1, NULL);
+
+	_tprintf(_T("Out Customer %d~ \n\n"), GetCurrentThreadId());
+};
+
+uint32 WINAPI ThreadProc(LPVOID lpParam)
+{
+	TakeMeal(reinterpret_cast<DWORD>(lpParam));
+
+	return 0;
+};
+
+int _tmain(int argc, TCHAR* argv[])
+{
+	std::array<DWORD, NUM_OF_CUSTOMER> dwThreadIDs;
+	std::array<HANDLE, NUM_OF_CUSTOMER> hThreads;
+
+	srand((uint32)time(NULL));
+
+	for (int i = 0; i < NUM_OF_CUSTOMER; ++i)
+	{
+		randTimeArr[i] = (DWORD)(
+			((double)rand() / (double)RAND_MAX)
+			* RANGE_MAX + RANGE_MIN);
+	};
+
+	hSemaphore = CreateSemaphore
+	(
+		NULL,
+		TABLE_CNT,
+		TABLE_CNT,
+		NULL);
+
+	if (hSemaphore == NULL)
+	{
+		_tprintf(_T("CreateSemaphore error : %d\n"), GetLastError());
+	};
+
+	for (int i = 0; i < NUM_OF_CUSTOMER; ++i)
+	{
+		hThreads[i] = (HANDLE)
+			_beginthreadex(
+				NULL, 0, ThreadProc, (void*)randTimeArr[i],
+				CREATE_SUSPENDED,
+				(unsigned*)&dwThreadIDs[i]);
+
+		if (hThreads[i] == NULL)
+		{
+			_tprintf(_T("Thread creation fault! \n"));
+			return -1; 
+		}
+	};
+
+	for (int i = 0; i < NUM_OF_CUSTOMER; ++i)
+	{
+		ResumeThread(hThreads[i]);
+	};
+
+	WaitForMultipleObjects(
+		NUM_OF_CUSTOMER, hThreads.data(), TRUE, INFINITE);
+
+	_tprintf(_T("=============END=================\n"));
+
+	for (int i = 0; i < NUM_OF_CUSTOMER; ++i)
+	{
+		CloseHandle(hThreads[i]);
+	};
+
+	CloseHandle(hSemaphore);
+	return 0;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- os_system_programming/os_system_programming/os_system_programming.cpp
+++ os_system_programming/os_system_programming/os_system_programming.cpp
@@ -1,20 +1,79 @@
-﻿// os_system_programming.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿#include <stdio.h>
+#include <tchar.h>
+#include <Windows.h>
+#include <process.h>
 
-#include <iostream>
+#define NUM_OF_GATE 6
+using uint32 = unsigned int;
+LONG gTotalCount = 0;
 
-int main()
+HANDLE hMutex;
+
+void IncreaseCount()
 {
-    std::cout << "Hello World!\n";
+	WaitForSingleObject(hMutex,INFINITE);
+
+	gTotalCount++;
+
+	ReleaseMutex(hMutex);
+};
+
+uint32 WINAPI ThreadProc(LPVOID lpParam)
+{
+	for (DWORD i = 0; i < 1000; ++i)
+	{
+		IncreaseCount();
+	}
+	return 0;
 }
 
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+int _tmain(int argc, TCHAR** argv)
+{
+	DWORD dwThreadID[NUM_OF_GATE];
+	HANDLE hThread[NUM_OF_GATE];
 
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
+	// InitializeCriticalSection(&hCriticalSection);
+
+	hMutex = 
+		CreateMutex(
+		NULL,
+		FALSE,
+		NULL);
+
+	for (DWORD i = 0; i < NUM_OF_GATE; ++i)
+	{
+		hThread[i] = (HANDLE)
+			_beginthreadex(
+				NULL, 0, ThreadProc,
+				NULL, CREATE_SUSPENDED,
+				(unsigned*)&dwThreadID[i]);
+
+		if (hThread[i] == NULL)
+		{
+			_tprintf(_T("Thread creation fault! \n"));
+			return -1; 
+		}
+	}
+
+	for (DWORD i = 0; i < NUM_OF_GATE; ++i)
+	{
+		ResumeThread(hThread[i]);
+	}
+
+	WaitForMultipleObjects(
+		NUM_OF_GATE, hThread, TRUE, INFINITE);
+
+	_tprintf(_T("total count : %d \n"), gTotalCount);
+
+	for (DWORD i = 0; i < NUM_OF_GATE; ++i)
+	{
+		CloseHandle(hThread[i]);
+	}
+
+	//DeleteCriticalSection(&hCriticalSection);
+	CloseHandle(hMutex);
+
+	return 0; 
+};
+
+

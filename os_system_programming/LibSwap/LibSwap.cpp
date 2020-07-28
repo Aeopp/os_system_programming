a267fbc5e9ab04324da95e9caf9eb961bd3a92d9
Aeop <irmxj777@gmail.com>
2020-07-28 20:19:55 +0900

    virtual memory heap mmf static library

---
+++ os_system_programming/LibSwap/LibSwap.cpp
@@ -0,0 +1,23 @@
+﻿#define _CRT_SECURE_NO_WARNING
+
+#include <stdio.h>
+#include <tchar.h>
+void swap(int* v1, int* v2);
+
+
+int _tmain(int argc, TCHAR* argv[])
+{
+
+	int a = 10;
+	int b = 20;
+	_tprintf(_T("Before: %d , %d \n"), a, b);
+
+	swap(&a, &b);
+	_tprintf(_T("After: %d, %d \n"), a, b);
+
+	return 0;
+
+};
+
+
+
---
+++ os_system_programming/SYSTEM_INFO/SYSTEM_INFO.cpp
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <tchar.h>
+#include <Windows.h>
+
+int _tmain(int argc, TCHAR* argv[])
+{
+	SYSTEM_INFO si;
+	DWORD allocGranularity;
+
+	DWORD pageSize;
+
+	GetSystemInfo(&si);
+
+	pageSize = si.dwPageSize;
+	allocGranularity = si.dwAllocationGranularity;
+
+	_tprintf(_T("Page size : %u KByte \n"), pageSize / 1024);
+	_tprintf(_T("Allocation granularity: %u KByte \n"),
+		allocGranularity / 1024);
+
+	return 1; 
+
+}
